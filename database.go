3268a0bc41fad506a7a02df02b27a71e3778e351
Massimiliano Salvemini <ms.salvemini@gmail.com>
2023-10-21 13:45:00 +0200

    database takes shape

---
+++ database.go
@@ -0,0 +1,10 @@
+package main
+
+// as per docker-compose.yml
+const (
+	host     = "localhost"
+	port     = 5438
+	user     = "counterblow_user"
+	password = "postgres123!?"
+	dbname   = "counterblow_db"
+)
--- sql/initdb.sql
+++ sql/initdb.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS rules (
   
   PRIMARY KEY (rule_id)
 );
+
+
+CREATE TABLE IF NOT EXISTS hits (
+  hit_from varchar(250) NOT NULL, -- ip addr
+  hit_to varchar(250) NOT NULL, -- routing address
+  hit_datetime timestamp without time zone default (now() at time zone 'utc') -- utc time so there is no confusion
+);
